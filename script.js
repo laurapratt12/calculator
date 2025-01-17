43acdc4064cc09a69fdc17acdd1138f3fa4c27ef
laurapratt12 <lauraprattico@hotmail.com>
2025-01-17 21:34:17 +0000

    selected elements

---
+++ script.js
@@ -0,0 +1,6 @@
+let output = document.getElementsByClassName("output");
+let clear = document.getElementById("clear");
+let operator = document.getElementById("operator");
+let deleteKey = document.getElementById("delete");
+let buttonNumbers = document.getElementsByClassName("btn cal-btns number");
+let enter = document.getElementById("enter")
